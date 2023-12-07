649aec5b11ccacf74a64bbc86cebbde696a45e93
hemanth <hemanth.kommoji@thetinydata.com>
2023-12-07 19:22:16 +0530

    enquiry-details-capture

--- src/app/features/enquiry-details/enquiry-routing.module.ts
+++ src/app/features/enquiry-details/enquiry-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppRoutePaths } from 'src/app/core/Constants';
 import { EnquiryDetailsComponent } from './enquiry-details/enquiry-details.component';
-
-
 const routes: Routes = [
   {
     path: '',
