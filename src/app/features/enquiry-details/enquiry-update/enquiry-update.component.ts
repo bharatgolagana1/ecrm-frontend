8b47b3337137e39d8b41682cfb99a60e3da5cde0
Bharat Yadav Golagana <bharat.golagana@gmail.com>
2023-12-08 10:17:40 +0530

    added stpper changes

--- src/app/features/enquiry-details/enquiry-update/enquiry-update.component.ts
+++ src/app/features/enquiry-details/enquiry-update/enquiry-update.component.ts
@@ -4,44 +4,40 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-enquiry-update',
   templateUrl: './enquiry-update.component.html',
-  styleUrls: ['./enquiry-update.component.scss']
+  styleUrls: ['./enquiry-update.component.scss'],
 })
 export class EnquiryUpdateComponent {
   public currentStep = 3;
 
   public step = [
     {
-      label:"Contact Details",
-      isValid:true
+      label: 'Contact Details',
+      isValid: true,
     },
     {
-      label:"Enquiry Details",
-      isValid:true
+      label: 'Enquiry Details',
+      isValid: true,
     },
     {
-      label: "Enquiry Description",
-      isValid:true
+      label: 'Enquiry Description',
+      isValid: true,
     },
     {
-      label: "Enquiry Update"
-    }
+      label: 'Enquiry Update',
+    },
   ];
-  public areaList: Array<string> = [
-    "Miami",
-    "New York",
-    "Philadelphia"
-  ];
+  public areaList: Array<string> = ['Miami', 'New York', 'Philadelphia'];
   public enquiryUpdateForm: FormGroup = new FormGroup({
-    POExpectedDate: new FormControl("", Validators.required),
-    dealPosition: new FormControl("", Validators.required),
-    probability: new FormControl("", Validators.required),
-    dealValue: new FormControl("", Validators.required),
-    modeOfCommunication: new FormControl("", Validators.required),
-    enterDescription: new FormControl("", Validators.required),
-    attachment:new FormControl("", Validators.required)
+    POExpectedDate: new FormControl('', Validators.required),
+    dealPosition: new FormControl('', Validators.required),
+    probability: new FormControl('', Validators.required),
+    dealValue: new FormControl('', Validators.required),
+    modeOfCommunication: new FormControl('', Validators.required),
+    enterDescription: new FormControl('', Validators.required),
+    attachment: new FormControl('', Validators.required),
   });
   public saveForm(): void {
     this.enquiryUpdateForm.markAllAsTouched();
-    console.log(this.enquiryUpdateForm.value)
+    console.log(this.enquiryUpdateForm.value);
   }
 }
