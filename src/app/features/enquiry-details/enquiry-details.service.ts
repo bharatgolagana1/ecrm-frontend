8d4473b32678e63d06327c73da42d336c5460444
Ganeshsamireddy <ganesh.samireddy@thetinydata.com>
2023-12-15 18:00:33 +0530

    enquiry-api

--- src/app/features/enquiry-details/enquiry-details.service.ts
+++ src/app/features/enquiry-details/enquiry-details.service.ts
@@ -6,11 +6,18 @@ import { AppSettingsConfigKey } from 'src/app/core/Constants';
 })
 export class EnquiryDetailsService {
   private loginUrl = `${AppSettingsConfigKey.APIURL}/api/Enquiry/GetLEContacts`;
+
+  private generatedFrom = `${AppSettingsConfigKey.APIURL}/api/Enquiry/GetGeneratedFrom`;
+  private salesWorkflow =  `${AppSettingsConfigKey.APIURL}/api/Enquiry/GetSalesWorkflow`;
+  private salesChannel   = `${AppSettingsConfigKey.APIURL}/api/Enquiry/GetSalesChannel`;
+  private generatedby = `${AppSettingsConfigKey.APIURL}/api/Enquiry/GetGeneratedBy`;
+  private quoteEntityCompany=`${AppSettingsConfigKey.APIURL}/api/Enquiry/GetQuoteCompany`;
   constructor(private http: HttpClient) {}
   getSoldToContactsList() {
     const url = `${this.loginUrl}`;
     return this.http.get(url);
   }
+<<<<<<< HEAD
   getSoldToSiteList(contactId: number) {
     const url = `${AppSettingsConfigKey.APIURL}/api/Enquiry/GetLESiteByContact`;
     const body = {
@@ -26,4 +33,26 @@ export class EnquiryDetailsService {
     };
     return this.http.post(url, body);
   }
+=======
+
+  getgeneratedFrom(){
+    return this.http.get(this.generatedFrom);
+  }
+
+  getsalesWorkFlow(){
+    return this.http.get(this.salesWorkflow);
+  }
+ getsalesChannel(){
+  return this.http.get(this.salesChannel)
+ }
+
+ postgeneratedBy(body : unknown){
+  return this.http.post(this.generatedby ,body)
+ }
+
+ getquoteEntityCompany(){
+  return this.http.get(this.quoteEntityCompany)
+ }
+
+>>>>>>> 55d1ff3 (enquiry-api)
 }
